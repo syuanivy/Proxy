e146ef1e5ddc6ff94f1d8e4a563b80d74d742d06
syuanivy <syuan6@usfca.edu>
2014-08-30 22:01:23 -0700

    Framework

---
+++ src/cs601/collections/ClientHandler.java
@@ -0,0 +1,33 @@
+package cs601.collections;
+
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+	Socket clientSocket;
+	
+	public ClientHandler(Socket clientSocket) { 
+		
+		this.clientSocket = clientSocket; 
+	}
+	
+	
+	//forwardRemoteDataToBrowser() 
+	
+	//getHeaders() 
+	
+	//makeUpstreamRequest() 
+	
+	//openUpstreamSocket() 
+	
+	//process() 
+	
+	//readLine() 
+	
+	//writeHeaders()
+	
+	public void run(){
+		
+	} 
+	
+
+}
---
+++ src/cs601/collections/ProxyServer.java
@@ -0,0 +1,13 @@
+package cs601.collections;
+
+public class ProxyServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
