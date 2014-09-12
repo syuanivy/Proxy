4789a31e2b08a555f8db8a69021ead673984b1d0
syuanivy <syuan6@usfca.edu>
2014-09-12 09:23:30 -0700

    Git directory problem solved maybe

--- src/cs601/proxy/ProxyServer.java
+++ src/cs601/proxy/ProxyServer.java
@@ -1,15 +1,31 @@
-package cs601.collections;
+package cs601.proxy;
+import java.io.IOException;
+import java.net.*;
+
 
 public class ProxyServer {
+	public static final int listeningPort= 8888;  // Port used to listen to requests
+	public static boolean openForService = true;
+	public static final boolean SingleThreaded = false;
+	
+	public static void main(String[] args) throws Exception {   
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServerSocket sSocket = new ServerSocket(listeningPort); //Open a server socket
+		while (openForService) {
+            try { 
+                Socket client = sSocket.accept(); //Wait for a socket connection
+                if (SingleThreaded){
+                	new ClientHandler(client).run(); // pass the client to ClientHandler and run
+                }
+                else{
+                   new Thread(new ClientHandler(client)).start();
+                }
+            }catch (IOException ioe) { 
+    			System.err.println("Can't complete the service!");
+    			ioe.printStackTrace(System.err);
+    		}
+        }
+		sSocket.close();
 	}
-
 }
-
 
